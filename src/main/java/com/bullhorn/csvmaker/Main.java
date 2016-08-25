163be86c05a86e8ce63543014c83b3ef12956704
Nathan Dickerson <ndickerson@bullhorn.com>
2016-08-25 14:30:03 -0500

    Added the ability to call the CSVMaker with command line parameters

--- src/main/java/com/bullhorn/csvmaker/Main.java
+++ src/main/java/com/bullhorn/csvmaker/Main.java
@@ -2,8 +2,24 @@ package com.bullhorn.csvmaker;
 
 public class Main {
 
+    public static void printUsage() {
+        System.out.println("Usage: java -jar target/CSVMaker-1.0.jar <EntityName> <NumRecords> <OutputDirectory>");
+        System.out.println("    where <EntityName> is one of the supported entities listed at:");
+        System.out.println("    https://github.com/bullhorn/dataloader/wiki/Supported-Entities");
+    }
+
     public static void main(String[] args) {
+        if (args.length < 2 || args.length > 3) {
+            printUsage();
+            return;
+        }
+
+        String outputDirectory = "csvOutput";
+        if (args.length > 2) {
+            outputDirectory = args[2];
+        }
+
         CSVMaker csvMaker = new CSVMaker();
-        csvMaker.createCSVFile("ENTITYNAME",1000);
+        csvMaker.createCSVFile(args[0], Integer.parseInt(args[1]), outputDirectory);
     }
 }
